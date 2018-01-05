b57ce2530cf1a2a872ddbbbed371a8e3f3638036
skakker <skakker@linkedin.com>
2018-01-05 14:31:29 +0530

    Update JvmUsedMemoryHeuristic.scala

--- app/com/linkedin/drelephant/spark/heuristics/JvmUsedMemoryHeuristic.scala
+++ app/com/linkedin/drelephant/spark/heuristics/JvmUsedMemoryHeuristic.scala
@@ -48,12 +48,12 @@ class JvmUsedMemoryHeuristic(private val heuristicConfigurationData: HeuristicCo
     )
 
     if(evaluator.severityExecutor.getValue > Severity.LOW.getValue) {
-      resultDetails :+ new HeuristicResultDetails("Note", "The allocated memory for the executor (in " + SPARK_EXECUTOR_MEMORY +") is much more than the peak JVM used memory by executors.")
+      resultDetails :+ new HeuristicResultDetails("Executor Memory", "The allocated memory for the executor (in " + SPARK_EXECUTOR_MEMORY +") is much more than the peak JVM used memory by executors.")
       resultDetails :+ new HeuristicResultDetails("Reasonable size for executor memory", ((1+BUFFER_PERCENT.toDouble/100.0)*evaluator.maxExecutorPeakJvmUsedMemory).toString)
     }
 
     if(evaluator.severityDriver.getValue > Severity.LOW.getValue) {
-      resultDetails :+ new HeuristicResultDetails("Note", "The allocated memory for the driver (in " + SPARK_DRIVER_MEMORY + ") is much more than the peak JVM used memory by the driver.")
+      resultDetails :+ new HeuristicResultDetails("Driver Memory", "The allocated memory for the driver (in " + SPARK_DRIVER_MEMORY + ") is much more than the peak JVM used memory by the driver.")
     }
 
     val result = new HeuristicResult(
